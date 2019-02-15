3bb00b603dacced9ea17466415c89d695255550a
Martin Saul <martin@saul.io>
2019-02-15 10:59:30 -0500

    Added order to tests.

--- integrationTest/src/test/java/io/connected/webtestclub/TodoIntegrationTest.java
+++ integrationTest/src/test/java/io/connected/webtestclub/TodoIntegrationTest.java
@@ -1,12 +1,15 @@
 package io.connected.webtestclub;
 
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TodoIntegrationTest {
 
 	private HttpCall httpCall;
@@ -17,10 +20,17 @@ public class TodoIntegrationTest {
 	}
 
 	@Test
-	public void validateEmpty() throws IOException {
+	public void A_validateEmpty() throws IOException {
 		HttpResponse result = httpCall.get("/todo/");
 		assertEquals("{\"message\":\"Ok!\",\"body\":[]}", result.getBody());
 		assertEquals(200, result.getHttpStatusCode());
+	}
+
+	@Test
+	public void B_create() throws IOException {
+		HttpResponse result = httpCall.post("/todo/", "{\"todo\":\"Hire a PO!\"}");
+		assertEquals("{\"message\":\"Created!\"}", result.getBody());
+		assertEquals(201, result.getHttpStatusCode());
 	}
 
 }
